7a60ce48389eacb9ee5f12751d5f9f21cea19972
rusyska55011 <ruslan.nikandrov2015@yandex.ru>
2021-09-22 15:06:48 +0300

    Add a comment upper method in terminal

--- terminal.py
+++ terminal.py
@@ -7,12 +7,15 @@ class Vk:
         self.vk_base = vk.VkBase()
         self.vk_bot = vk.VkBot(access_token=VkSettings.access_token)
 
+    # Запускает чат бота
     def start_chat_bot(self):
         vk.LongPool(self.vk_bot, VkSettings.chat_bot_asks).start()
 
+    # Запускает автоматическую рассылку новым друзьям
     def start_friend_checker(self):
         vk.AddUsersChecker(self.vk_bot, VkSettings.message_for_added_users, self.vk_bot.get_friends_list()).start()
 
+    # Пушит сообщения всем пользователям, записанных в базу данных!
     def push(self, message: str=None):
         if not message:
             message = VkSettings.default_pushing_message
@@ -29,16 +32,22 @@ class Vk:
             else:
                 print('Введи корректное значение')
 
+    # Делает запись в бд зная id, имя вводит самостоятельно
+    # Принимает неограниченное число чисел (id)
     def add_by_id(self, *args: int):
         for vk_id in args:
             self.vk_bot.add_by_id(vk_id)
 
+    # Напрямую делает запись в дазу даннхы
+    # Принимает неограниченное число массивов, в каждом массиве должно быть ТОЛЬКО 2 значения ['id', 'Имя']
     def add(self, *args: list):
         self.vk_base.add(*args)
 
+    # Удаляет элементы из БД
     def delete(self, *args: int):
         self.vk_base.delete(*args)
 
+    # Выводит список элементов в бд
     def read(self):
         print(self.vk_base.get_col_names())
         for vk_id, name in self.vk_base.read():
